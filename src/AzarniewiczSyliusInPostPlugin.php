b96eecb4a769e2a5e66bcd01433741edf8380a9a
Adam Żarniewicz <a.zarniewicz@gmail.com>
2026-03-25 10:50:20 +0100

    Fix bundle extension alias registration

--- src/AzarniewiczSyliusInPostPlugin.php
+++ src/AzarniewiczSyliusInPostPlugin.php
@@ -4,8 +4,14 @@ declare(strict_types=1);
 
 namespace Azarniewicz\SyliusInPostPlugin;
 
+use Azarniewicz\SyliusInPostPlugin\DependencyInjection\AzarniewiczSyliusInPostPluginExtension;
+use Symfony\Component\DependencyInjection\Extension\ExtensionInterface;
 use Symfony\Component\HttpKernel\Bundle\Bundle;
 
 final class AzarniewiczSyliusInPostPlugin extends Bundle
 {
+    public function getContainerExtension(): ?ExtensionInterface
+    {
+        return $this->extension ??= new AzarniewiczSyliusInPostPluginExtension();
+    }
 }
